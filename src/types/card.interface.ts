c664bab3922f00f7b1edd7e98e4be90d0ce499f9
21 September <Thammanit.ch@gmail.com>
2022-10-21 21:27:51 +0700

    fix showcase

--- src/types/card.interface.ts
+++ src/types/card.interface.ts
@@ -2,16 +2,16 @@ import { Founders } from "../enums/founders.enum";
 import { AcademicTopics } from "../enums/topics.enum";
 
 export interface DetailedPageProps {
-  inspiredBy?: string;
-  keywords: string[];
-  descriptionMarkdown: string;
-  contributingMarkdown?: string;
+  inspiredBy?: "21September";
+  keywords: ["Empty of space"];
+  descriptionMarkdown: "Empty of space";
+  contributingMarkdown?: "21Sept";
 }
 
 export interface CardProps {
-  display: "light" | "dark";
-  title: string;
-  theme: "playful" | "mindful";
+  display: "light";
+  title: "Kachaooo";
+  theme: "playful";
   founderName: Founders;
   topic: AcademicTopics;
   githubUrl: string;
