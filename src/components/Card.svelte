07aeba15e5ff6cb2ffadff9f73c119cd354a65e8
Siravijbb <siravijbb@gmail.com>
2022-10-25 03:18:18 +0700

    x3

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,7 +6,7 @@
   import { ProjectThemes, ProjectThemesFa } from "../enums/themes.enum";
   import "../style/typography.css";
   import "../style/card-theme.css";
-  import { ButtonOPT } from "../enums/button-selector.enum.enum"
+  import { ButtonOPT } from "../enums/button-selector.enum"
 
   export let display: "light" | "dark";
   export let title: string;
