06b89426c0d3d8da94273fd88168d56ce156d109
Siravijbb <siravijbb@gmail.com>
2022-10-25 03:41:40 +0700

    try harder

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -15,7 +15,7 @@
   export let topic: AcademicTopics;
   export let githubUrl: string;
   export let imagePath: string | undefined;
-  export let ButtonO: string | undefined;
+  export let ButtonO: string;
 
   const colorSet =
     display == "light"
