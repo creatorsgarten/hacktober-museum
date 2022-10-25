b853961752ef7c0bbe72759ac03c8828ec0e0d6d
Siravijbb <siravijbb@gmail.com>
2022-10-25 22:57:59 +0700

    fixed bug

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,7 +6,7 @@
   import { ProjectThemes, ProjectThemesFa } from "../enums/themes.enum";
   import "../style/typography.css";
   import "../style/card-theme.css";
-  import { ButtonOPT } from "../enums/button-selector"
+  import {  ButtoDisplayoption  } from "../emum/button-selector.enum"
 
   export let display: "light" | "dark";
   export let title: string;
@@ -76,7 +76,7 @@
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                 {ButtonOPT}
+                 {ButtoDisplayoption}
               </a>
             </div>
           </div>
