4b00e9096d952af1f226f181b763d5dda78c44a5
Siravijbb <siravijbb@gmail.com>
2022-10-29 14:13:21 +0700

    fixed bug

--- src/entries/easyeda.easyeda18.ts
+++ src/entries/easyeda.easyeda18.ts
@@ -5,12 +5,12 @@ import { CardProps } from "../types/card.interface";
 
 const props: CardProps = {
   display: "dark",
-  title: "EasyEDA2021-i18n",
+  title: "EasyEDA - PCB Design",
   theme: ProjectThemes.Mindful,
   founderName: Founders.EasyEDA2021,
   topic: AcademicTopics.JavaScript,
   githubUrl: "https://github.com/EasyEDA2021",
-  imagePath: "94C6FB5B-8587-4175-8224-82F5D961249F.jpeg"
+  imagePath: undefined
 };
 
 export default props;
