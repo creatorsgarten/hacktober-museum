e04b27d72a2b1bee715b913511f8a4aefc34e602
Siravijbb <siravijbb@gmail.com>
2022-11-01 00:15:24 +0700

    CLRF

--- src/entries/Universal-wireless-communication-library.ts
+++ src/entries/Universal-wireless-communication-library.ts
@@ -2,6 +2,8 @@ import { Founders } from "../enums/founders.enum";
 import { ProjectThemes } from "../enums/themes.enum";
 import { AcademicTopics } from "../enums/topics.enum";
 import { CardProps } from "../types/card.interface";
+
+
 const props: CardProps = {
   display: "dark",
   title: "Universal wireless communication library",
