ad8881923016d0a52716cc1dfd238518230e6c0e
Siravijbb <siravijbb@gmail.com>
2022-11-01 00:20:59 +0700

    tem

--- src/entries/Universal-wireless-communication-library.ts
+++ src/entries/Universal-wireless-communication-library.ts
@@ -3,7 +3,6 @@ import { ProjectThemes } from "../enums/themes.enum";
 import { AcademicTopics } from "../enums/topics.enum";
 import { CardProps } from "../types/card.interface";
 
-
 const props: CardProps = {
   display: "dark",
   title: "Universal wireless communication library",
@@ -11,7 +10,8 @@ const props: CardProps = {
   founderName: Founders.jgromes,
   topic: AcademicTopics.Arduino,
   imagePath: "/images/jgromes.png",
-  githubUrl: "https://github.com/RadioLib",
+  githubUrl: "https://github.com/jgromes/RadioLib",
 };
 
 export default props;
+
